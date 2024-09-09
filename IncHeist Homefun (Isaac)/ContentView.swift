e7e9a14b8fa3987934384e116159db3be969200d
Isaac <isaac_woon_xin_wei@s2024.ssts.edu.sg>
2024-09-09 17:30:20 +0800

    gay

--- IncHeist Homefun (Isaac)/ContentView.swift
+++ IncHeist Homefun (Isaac)/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, worldsdgsIm gay!")
+            Text("Hello, gaygaygsIm gay!")
         }
         .padding()
     }
